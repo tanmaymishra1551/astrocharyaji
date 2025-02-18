824a1ac62e51c4b60976f8e7cfb66f5ea18a5e9e
tanmaymishra1551 <tanmaymishra1551@gmail.com>
2025-02-18 20:03:09 +0530

    Add vercel backend url

--- my-astrology-app/vite.config.js
+++ my-astrology-app/vite.config.js
@@ -8,13 +8,13 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://astrobackend.vercel.app/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
       "/wsapi": {
-        target: "http://localhost:8000",
+        target: "https://astrobackend.vercel.app/",
         ws: true,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/wsapi/, ""),
